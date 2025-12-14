1b02aa49caa3c915ff18df2c4b1e27b5188d2fb2
vikkiroxx <singh.vikramchauhan28@gmail.com>
2025-12-14 14:55:42 +0530

    feat: Configure Deployment to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/CredTrack/' : '/',
   plugins: [
     react(),
     VitePWA({
@@ -25,4 +26,4 @@ export default defineConfig({
       }
     })
   ],
-})
+}))
