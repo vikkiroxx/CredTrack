70a2264113a91c2b86e5639ab742634699ef6a67
vikkiroxx <singh.vikramchauhan28@gmail.com>
2025-12-18 00:04:10 +0530

    feat: update app icon to square version

--- src/App.tsx
+++ src/App.tsx
@@ -43,7 +43,7 @@ function AppContent() {
   const [isSettingsOpen, setIsSettingsOpen] = useState(false);
 
   useEffect(() => {
-    console.log("CredTrack Version: 0.1.2 Loaded");
+    console.log("CredTrack Version: 0.1.3 Loaded");
     const handleHashChange = () => {
       setView(getViewFromHash());
     };
