1b8c64c5f3a5e1f4b0adb80bc3599168793391c7
vikkiroxx <singh.vikramchauhan28@gmail.com>
2025-12-27 08:32:49 +0530

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/CredTrack/sw.js', { scope: '/CredTrack/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
